c531c597e0f82d4fb04d5aed870935c16f05d16d
oku3san <takaya0303@gmail.com>
2024-03-20 16:05:24 +0900

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto_git_pusher',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'auto_git_pusher = package.main:auto_git_pusher',
+        ],
+    },
+)
