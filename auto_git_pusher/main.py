4c78df7bed4ca7052a80323238b4981fef4a2d79
oku3san <takaya0303@gmail.com>
2024-03-20 16:51:42 +0900

    Update

--- auto_git_pusher/main.py
+++ auto_git_pusher/main.py
@@ -1,16 +1,14 @@
-import fire
 from git import Repo
 
 
-# auto_git_pusher function
-# git add -A && git commit -m "Update" && git push origin HEAD
 def auto_git_pusher(repo_path=''):
     repo = Repo(repo_path)
     repo.git.add('-A')
     repo.index.commit("Update")
-    repo.remotes.origin.push(repo.head.ref)
+    repo.remotes.origin.push()
 
 
-# main.py
 if __name__ == "__main__":
-    fire.Fire(auto_git_pusher)
+    from fire import Fire
+
+    Fire(auto_git_pusher)
