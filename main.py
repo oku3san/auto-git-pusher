1b1984fccd0edb2fee0c3ed623be1e15128f5715
oku3san <takaya0303@gmail.com>
2024-03-20 11:53:27 +0900

    Update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def git_add_all(repo_path=os.getcwd()):
     repo = Repo(repo_path)
     repo.git.add('-A')
     repo.index.commit("Update")
+    repo.remotes.origin.push(repo.head.ref)
 
 
 if __name__ == "__main__":
