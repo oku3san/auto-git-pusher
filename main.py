062d58b8df04be54a41878ff7ac92f4b1af932c1
oku3san <takaya0303@gmail.com>
2024-03-20 11:52:33 +0900

    Update

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import fire
+from git import Repo
+
+
+def git_add_all(repo_path=os.getcwd()):
+    repo = Repo(repo_path)
+    repo.git.add('-A')
+    repo.index.commit("Update")
+
+
+if __name__ == "__main__":
+    fire.Fire(git_add_all)
